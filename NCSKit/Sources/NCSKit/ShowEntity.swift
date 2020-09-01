adb92c3b20be8f35b411ca90094236f6a888a616
Leo Dion <leogdion@brightdigit.com>
2020-09-01 17:40:43 -0400

    working on import functionality

--- NCSKit/Sources/NCSKit/ShowEntity.swift
+++ NCSKit/Sources/NCSKit/ShowEntity.swift
@@ -8,6 +8,37 @@
 import Foundation
 import CoreStore
 
+enum Errors : Error {
+  case missingRequired(String)
+  case invalidConversion(Any)
+}
+
+
+extension Dictionary where Key == String, Value == Any {
+  func get<ValueType>(_ valueType: ValueType.Type, key: String) throws -> ValueType? {
+    guard let anyValue = self[key] else {
+      return nil
+    }
+    
+    guard let value = anyValue as? ValueType else {
+      throw Errors.invalidConversion(anyValue)
+    }
+    
+    return value
+  }
+  
+  func required<ValueType>(_ valueType: ValueType.Type, key: String) throws -> ValueType {
+    guard let value = try self.get(valueType, key: key) else {
+      throw Errors.missingRequired(key)
+    }
+    return value
+  }
+  
+//  func get<ValueType>(key: String, _ defaultValue: ValueType) throws -> ValueType {
+//    return self.get(type(of: defaultValue), key: key) ?? defaultValue
+//  }
+}
+
 extension Entity where O : NamedObject {
   convenience init() {
     self.init(O.self.entityName)
@@ -25,7 +56,24 @@ public class ServiceEntity : CoreStoreObject, NamedObject {
   public var shows : Set<ShowEntity>
 }
 
-public class ShowEntity : CoreStoreObject, NamedObject {
+public class ShowEntity : CoreStoreObject, NamedObject, ImportableUniqueObject {
+  public static let uniqueIDKeyPath: String = #keyPath(ShowEntity.id)
+  
+  public static func uniqueID(from source: [String : Any], in transaction: BaseDataTransaction) throws -> Int? {
+    return source["id"] as? Int
+  }
+  
+  public func update(from source: [String : Any], in transaction: BaseDataTransaction) throws {
+    self.id = try source.required(Int.self, key: "id")
+    self.title = try source.required(String.self, key: "title")
+    self.imageURL = try source.required(URL.self, key: "imageURL")
+    //        self.exchanges = try! NSSet(array: transaction.importUniqueObjects(Into<Exchange>(), sourceArray: source["exchanges"].array!))
+  }
+  
+  public typealias UniqueIDType = Int
+  
+  public typealias ImportSource = [ String : Any ]
+  
   public static let entityName = "Show"
   
   @objc
@@ -58,9 +106,10 @@ extension EpisodeStatus : FieldStorableType {
   
   
 }
-public class EpisodeEntity : CoreStoreObject, NamedObject {
+public class EpisodeEntity : CoreStoreObject, NamedObject, ImportableUniqueObject {
   public static let entityName = "Episode"
   
+  @objc
   @Field.Stored("id")
   public var id : Int = -1
   @Field.Stored("number")
@@ -75,4 +124,24 @@ public class EpisodeEntity : CoreStoreObject, NamedObject {
   public var status: EpisodeStatus = .draft
   @Field.Relationship("show")
   public var show : ShowEntity?
+  
+  public static let uniqueIDKeyPath: String = #keyPath(EpisodeEntity.id)
+  
+  public static func uniqueID(from source: [String : Any], in transaction: BaseDataTransaction) throws -> Int? {
+    return source["id"] as? Int
+  }
+  
+  public func update(from source: [String : Any], in transaction: BaseDataTransaction) throws {
+    self.id = try source.required(Int.self, key: "id")
+    self.title = try source.required(String.self, key: "title")
+    self.mediaURL = try source.get(URL.self, key: "mediaURL")
+    self.number = try source.get(Int.self, key: "URL")
+    self.summary = try source.get(String.self, key: "summary")
+    self.status = try source.required(EpisodeStatus.self, key: "status")
+    
+  }
+  
+  public typealias UniqueIDType = Int
+  
+  public typealias ImportSource = [ String : Any ]
 }
